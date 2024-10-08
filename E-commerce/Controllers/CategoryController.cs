15429cbea877826f8fd806a28ce145bf9c2280d3
Assem Khashaba <assemkhashaba0@gmail.com>
2024-10-08 15:23:37 +0300

    Add project files.

---
+++ E-commerce/Controllers/CategoryController.cs
@@ -0,0 +1,68 @@
+﻿using E_commerce.Data;
+using E_commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols;
+
+namespace E_commerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var categorys = dbContext.Categories.ToList();
+            return View(model: categorys);
+        }
+
+        public IActionResult Newcategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Newcategory(Category category)
+        {
+            dbContext.Categories.Add(category);
+            dbContext.SaveChanges();
+            
+            return RedirectToAction("Index"); 
+
+
+        }
+        public IActionResult Edit(int categoryId)
+        {
+            var category = dbContext.Categories.Find(categoryId);
+            if (category != null)
+            {
+                return View(category);
+            }
+            return RedirectToAction("NotFoundPage", "Home");
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            dbContext.Categories.Update(category);
+            dbContext.SaveChanges();
+            
+             return RedirectToAction(nameof(Index));
+
+            
+
+
+        }
+        [HttpGet]
+
+        public IActionResult Delete(int id)
+        {
+            Category category = new Category() { Id = id };
+            dbContext.Categories.Remove(category);
+            dbContext.SaveChanges();
+           
+
+            return RedirectToAction("Index");
+
+
+        }
+
+    }
+}
---
+++ E-commerce/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using E_commerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerce.Controllers
+{
+    public class ProductController : Controller
+    {
+      ApplicationDbContext dbcontext = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var products = dbcontext.products.ToList();
+            return View(products);
+        }
+    }
+}
