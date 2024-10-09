e666b57181698af469e1c4b938c93285b7841d6d
Assem Khashaba <assemkhashaba0@gmail.com>
2024-10-09 16:04:01 +0300

    Finished reading, adding, and deleting items in the product.

--- E-commerce/Controllers/ProductController.cs
+++ E-commerce/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using E_commerce.Data;
+using E_commerce.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,8 +10,50 @@ namespace E_commerce.Controllers
       ApplicationDbContext dbcontext = new ApplicationDbContext();
         public IActionResult Index()
         {
-            var products = dbcontext.products.ToList();
+            var products = dbcontext.products.Include(e => e.Category).ToList();
             return View(products);
+        }
+        [HttpGet]
+
+        public IActionResult Newproduct()
+        {
+            var products = dbcontext.Categories.ToList();
+            return View(products);
+        }
+        [HttpPost]
+        public IActionResult Newc(Product product ,IFormFile ImgUrl)
+        
+        {
+            if (ImgUrl.Length > 0)
+            {
+                var FileName = ImgUrl.FileName;
+                var FilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot//images", FileName);
+
+                using (var Stream = System.IO.File.Create(FilePath))
+                {
+                    ImgUrl.CopyTo(Stream);
+                }
+                product.ImgUrl = ImgUrl.FileName;
+            }
+
+
+           dbcontext.products.Add(product);
+            dbcontext.SaveChanges();
+
+            return RedirectToAction("Index");
+
+
+        }
+        public IActionResult Delete(int id)
+        {
+            Product product  = new Product() { Id = id };
+            dbcontext.products.Remove(product);
+            dbcontext.SaveChanges();
+
+
+            return RedirectToAction("Index");
+
+
         }
     }
 }
